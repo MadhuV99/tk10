1ef12f9cddca87f11978dc1f49024e8f61895634
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-02-04 21:31:59 -0600

    Latest Commit

---
+++ basics/windows.py
@@ -0,0 +1,20 @@
+# windows basics
+import tkinter
+
+#Define window
+root = tkinter.Tk()
+root.title('Windows Basics!')
+root.iconbitmap('thinking.ico')
+root.geometry('300x700')
+root.resizable(0, 0)
+root.config(bg='blue')
+
+
+#Second window
+top = tkinter.Toplevel()
+top.title('Second Window')
+top.geometry('275x350+500+50')
+top.config(bg='#123456')
+
+#Run root window's main loop
+root.mainloop()
