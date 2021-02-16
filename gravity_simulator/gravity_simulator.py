5e2c222b4847dbf12bb20aae93e0fb243cd5692c
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-02-15 21:38:52 -0600

    latest commit

---
+++ gravity_simulator/gravity_simulator.py
@@ -0,0 +1,19 @@
+#Gravity Simulator
+import tkinter
+from tkinter import BOTH, HORIZONTAL, CURRENT, END
+# from matplotlib import pyplot
+
+#Define window
+root = tkinter.Tk()
+root.title('Gravity Simulator')
+root.iconbitmap('earth.ico')
+root.geometry('500x650')
+root.resizable(0,0)
+
+#Define fonts and colors
+#NONE use system defualts
+
+
+
+#Run root window's main loop
+root.mainloop()
