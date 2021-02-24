95f1a70016d494dd905913bfbc12ca4f9cc6efb7
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-02-23 19:47:32 -0600

    latest commit

--- crypto_cap/crypto_cap.py
+++ crypto_cap/crypto_cap.py
@@ -1,7 +1,9 @@
 # crypto_cap
 import requests, json
 
-api_requests = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=42796342-84df-4c6b-8e36-1bb0b7e9c2ac") 
+# api_requests = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=SECRET-KEY") 
+api_requests = requests.get("https://api.coinmarketcap.com/v1/ticker/") 
+
 api = json.loads(api_requests.content)
 print(api)
 
